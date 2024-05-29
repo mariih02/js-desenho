d78e24cccb8c6b3f7c55b447730d6243f8fb24fc
mariih02 <marianasilvasantana90@gmail.com>
2024-05-29 10:38:38 -0300

    dd

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(600, 400);
+  }
+  
+  function draw() {
+    background("black");
+    
+    stroke("white")
+    fill("red")
+    rect(20,0,100,100)
+  }
+    
